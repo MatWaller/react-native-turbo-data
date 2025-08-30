c465bfe1940838ab1b6a4e6ae77ac57799895a3b
MatWaller <wallermadev@gmail.com>
2025-08-30 22:20:46 +0100

    style: updating spec

--- specs/NativeTurboData.ts
+++ specs/NativeTurboData.ts
@@ -1,18 +1,26 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
+export interface FilterableRecord {
+  value: {
+    stringValue?: string;
+    numberValue?: number;
+    booleanValue?: boolean;
+  };
+  field: string;
+}
+
+export interface SortConfig {
+  field: string;
+  direction: string;
+}
+
 export interface Spec extends TurboModule {
   filterObject(
-    dataObject: readonly {
-      [key: string]: string | number | boolean | null | object;
-    }[],
-    filterCriteria: {
-      [key: string]: string | number | boolean | null | object;
-    },
-    sortCriteria?: { [key: string]: 'asc' | 'desc' }
-  ): readonly {
-    [key: string]: string | number | boolean | null | object;
-  }[];
+    dataObject: Array<FilterableRecord>,
+    filterCriteria: FilterableRecord,
+    sortCriteria?: SortConfig
+  ): Array<FilterableRecord>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('NativeTurboData');
